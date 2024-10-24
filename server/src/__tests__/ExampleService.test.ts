4ba2a76b9ee6f0a42ef2d041f312f7e68e1e7269
Florence Buchelet <florence.buchelet@gmail.com>
2024-10-24 17:04:20 +0200

    TU mock data ok

--- server/src/__tests__/ExampleService.test.ts
+++ server/src/__tests__/ExampleService.test.ts
@@ -1,16 +1,17 @@
-import { Category } from "../entities/Category";
+jest.mock("../services/NotificationService");
+
 import { Example } from "../entities/Example";
 import { ExampleService } from "../services/ExampleService";
 
 describe("ExampleService", () => {
 
+    let exampleService: ExampleService;
+
+    beforeEach(() => {
+        exampleService = new ExampleService();
+    });
+
     describe("joinExampleTitles", () => {
-
-        let exampleService: ExampleService;
-
-        beforeEach(() => {
-            exampleService = new ExampleService();
-        });
 
         it("should return an empty string if there are no examples", () => {
             const joined: string = exampleService.joinExampleTitles([]);
@@ -24,8 +25,16 @@ describe("ExampleService", () => {
                 new Example("ple")
             ]
             const joined: string = exampleService.joinExampleTitles(example);
-            console.log(example)
             expect(joined).toBe("Example");
+        })
+    })
+
+    describe("sendNotification", () => {
+
+        it("should send a notification", () => {
+            const example = new Example("Example");
+            const sent = exampleService.sendNotification(example);
+            expect(sent).toBe(undefined);
         })
     })
 });
--- server/src/services/ExampleService.ts
+++ server/src/services/ExampleService.ts
@@ -1,6 +1,15 @@
 import type { Example } from "../entities/Example";
+import { NotificationService } from "../services/NotificationService";
 
 export class ExampleService {
+
+    private readonly notificationService: NotificationService;
+
+    constructor(
+        notificationService: NotificationService = new NotificationService()
+    ) {
+        this.notificationService = notificationService;
+    }
 
     joinExampleTitles(examples: Example[]): string {
         if (examples.length === 0) {
@@ -9,6 +18,10 @@ export class ExampleService {
 
         const joined: string = examples.map(example => example.title).join("");
         return joined;
+    }
+
+    sendNotification(example: Example): void {
+        this.notificationService.sendEmail(example.title, "some guy");
     }
 
 }
