3bc2404c18b021125f60d7a1e5d25f0ea0db12bf
Florence Buchelet <florence.buchelet@gmail.com>
2024-11-28 18:33:58 +0100

    header

--- client/src/layouts/Footer.tsx
+++ client/src/layouts/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<>
 			<footer className="footer">
-				<p>Footer</p>
+				<p>__</p>
 			</footer>
 		</>
 	);
--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
 	return (
 		<>
 			<header className="header">
-				<h1>Header</h1>
+				<h1>Bonjour User !</h1>
 			</header>
 		</>
 	);
