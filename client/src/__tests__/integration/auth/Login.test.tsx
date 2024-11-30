cbd8bddc3e423c8c3e3cc78c9161026bf47c4059
Florian Schaessens <f.schaessens@gmail.com>
2024-11-30 12:48:52 +0100

    add EmptyFields Case for Login.test

--- client/src/__tests__/integration/auth/Login.test.tsx
+++ client/src/__tests__/integration/auth/Login.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import { http, HttpResponse } from 'msw';
-import { setupServer } from 'msw/node';
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 
 import "@testing-library/jest-dom";
 
@@ -31,19 +31,18 @@ describe("LoginForm", () => {
 
 	it("should send error message when fields are empty", async () => {
 		render(<LoginForm />);
-		
+
 		const loginButton = await screen.findByRole("button", {
 			name: "Me connecter",
 		});
 
 		userEvent.click(loginButton);
 
-		// Vérifie qu'un message d'erreur est affiché pour chaque champ vide 
+		// Vérifie qu'un message d'erreur est affiché pour chaque champ vide
 		const emailError = await screen.findByText(/L'email est requis/);
 		const passwordError = await screen.findByText(/Le mot de passe est requis/);
-	
+
 		expect(emailError).toBeInTheDocument();
 		expect(passwordError).toBeInTheDocument();
-
-	})
+	});
 });
