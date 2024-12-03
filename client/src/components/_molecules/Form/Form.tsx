55903aa81636ab45221e562b960c79ccd7a98b55
Florence Buchelet <florence.buchelet@gmail.com>
2024-12-03 11:51:19 +0100

    review

--- client/src/components/_molecules/Form/Form.tsx
+++ client/src/components/_molecules/Form/Form.tsx
@@ -7,18 +7,22 @@ export default function Form({
 	title,
 	children,
 }: { title: string; children: React.ReactNode }) {
+
+	// Read { children } as an array to filter elements
 	const buttons = React.Children.toArray(children).filter(
 		(child) => React.isValidElement(child) && child.type === Button,
 	);
+
 	const inputs = React.Children.toArray(children).filter(
 		(child) => React.isValidElement(child) && child.type === TextInput,
 	);
 
-	const fields = React.Children.toArray(children).filter(
+	const otherFields = React.Children.toArray(children).filter(
 		(child) =>
 			!React.isValidElement(child) ||
 			(child.type !== Button && child.type !== TextInput), // Tout sauf les `Button`
 	);
+	// -- End children filtering
 
 	return (
 		<>
@@ -26,7 +30,7 @@ export default function Form({
 				<h2>{title}</h2>
 				<div>{inputs}</div>
 				<div className="form__buttons">{buttons}</div>
-				<div className="form__fields">{fields}</div>
+				<div className="form__otherFields">{otherFields}</div>
 			</form>
 		</>
 	);
--- client/src/layouts/Footer.tsx
+++ client/src/layouts/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<>
 			<footer className="footer">
-				<p>__</p>
+				<p>Footer</p>
 			</footer>
 		</>
 	);
