a616c5c578c7f9cf3a6e35624688c04e8ab87b12
Florence Buchelet <florence.buchelet@gmail.com>
2024-12-04 12:08:28 +0100

    todo list login form

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./styles/global.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage.tsx";
-import Layout from "./layouts/Layout.tsx";
 import PageName from "./pages/PageName/PageName.tsx";
 import DesignSystem from "./pages/DesignSystem/DesignSystem.tsx";
+import Login from "./pages/Login/Login.tsx";
 
 const router = createBrowserRouter([
 	{
@@ -20,13 +20,15 @@ const router = createBrowserRouter([
 		path: "/designsystem",
 		element: <DesignSystem />,
 	},
+	{
+		path: "/login",
+		element: <Login />,
+	}
 ]);
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Layout>
-			<RouterProvider router={router} />
-		</Layout>
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
---
+++ client/src/pages/Login/Login.tsx
@@ -0,0 +1,22 @@
+import "./Login.scss";
+
+function Login() {
+    return (
+        /**
+         * logo + titre paw planner
+         * formulaire
+         *     - email
+         *     - password
+         *     - bouton login
+         *     - deux lignes de plus  
+         *          - oubli mot de passe
+         *          - inscription
+         * image ?
+         * */
+        <>
+            <h1>login</h1>
+        </>
+    );
+}
+
+export default Login;
