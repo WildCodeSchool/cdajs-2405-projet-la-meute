d4a43d2c8794e0747435760a5107507d35e296f3
Florence Buchelet <florence.buchelet@gmail.com>
2024-12-04 11:49:58 +0100

    format

--- client/src/pages/PageName/PageName.tsx
+++ client/src/pages/PageName/PageName.tsx
@@ -4,7 +4,6 @@ import { GET_EXAMPLE_BY_ID } from "@/graphQL/queries/example";
 import { useQuery } from "@apollo/client";
 
 function PageName() {
-
 	const { data, loading, error } = useQuery(GET_EXAMPLE_BY_ID, {
 		variables: {
 			getExampleByIdId: 1,
@@ -12,13 +11,14 @@ function PageName() {
 	});
 
 	if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
+	if (error) return <p>Error: {error.message}</p>;
 
 	return (
 		<main className="debug">
 			<h1 className="pageName__element--modifier">Page Name</h1>
 			<a href="/" className="debug">
-				Test fetch should show an example : {data?.getExampleById ? `🐾 ${data.getExampleById.title} 🐾` : "failed"}
+				Test fetch should show an example :
+				{data?.getExampleById ? `🐾 ${data.getExampleById.title} 🐾` : "failed"}
 			</a>
 			<ComponentName />
 		</main>
