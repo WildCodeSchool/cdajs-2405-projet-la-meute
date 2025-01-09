ba30aa227384309e89a5f0b6b5e53ab591f54728
lgrignon <louis.grignon@conscience-robotics.com>
2025-01-09 14:54:33 +0100

    added full calendar just as a demo

--- client/src/pages/Planning/Planning.tsx
+++ client/src/pages/Planning/Planning.tsx
@@ -1,7 +1,14 @@
 import "./Planning.scss";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 function Planning() {
-	return <></>;
+	return (
+		<FullCalendar
+		  plugins={[ dayGridPlugin ]}
+		  initialView="dayGridMonth"
+		/>
+	  )
 }
 
 export default Planning;
