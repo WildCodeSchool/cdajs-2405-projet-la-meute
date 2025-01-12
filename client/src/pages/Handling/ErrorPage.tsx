256bca148aa895d16e302bf6f860670a560daa90
Florian Schaessens <f.schaessens@gmail.com>
2025-01-12 14:58:14 +0100

    feature error handling page

---
+++ client/src/pages/Handling/ErrorPage.tsx
@@ -0,0 +1,35 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+import { Link } from "react-router-dom";
+import dog404 from "@/assets/illustrations/dog404.png";
+import "./ErrorPage.scss";
+
+export default function ErrorPage() {
+	const error = useRouteError();
+	let title = "";
+	let is404 = false;
+
+	if (isRouteErrorResponse(error)) {
+		title = `Erreur ${error.status}`;
+		is404 = error.status === 404;
+	}
+
+	return (
+		<main className="notfound">
+			<hgroup>
+				<h1>Oups !</h1>
+				{title && <p>{title}</p>}
+				{is404 ? (
+					<p>
+						La page que vous recherchez <br /> semble introuvable.
+					</p>
+				) : (
+					<p>
+						Une erreur inattendue <br /> s'est produite
+					</p>
+				)}
+				<Link to="/">Retour à l'accueil</Link>
+			</hgroup>
+			<img src={dog404} alt="Chien débranchant une prise" />
+		</main>
+	);
+}
