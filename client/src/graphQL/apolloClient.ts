7916c6a1922ddd8adc33d4e958ff304078f0ebd0
Florian Schaessens <f.schaessens@gmail.com>
2025-01-20 15:50:57 +0100

    add cors policy

--- client/src/graphQL/apolloClient.ts
+++ client/src/graphQL/apolloClient.ts
@@ -1,8 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const isProd = import.meta.env.VITE_NODE_ENV === "production";
+
 const client = new ApolloClient({
-	uri: "http://localhost:3200/",
+	uri: isProd ? "/graphql" : "http://localhost:3200",
 	cache: new InMemoryCache(),
+	credentials: isProd ? "include" : "omit",
 });
 
 export default client;
