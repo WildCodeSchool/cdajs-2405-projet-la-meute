cfc861e8308f6b7707b56bda15a93c792479e191
Julien PRIVAT <contact@julien-privat.com>
2025-02-01 23:25:02 +0100

    CSS partie desktop et mobile

--- client/src/pages/Planning/PlanningTrainer.tsx
+++ client/src/pages/Planning/PlanningTrainer.tsx
@@ -2,7 +2,7 @@ import "@/pages/Planning/PlanningTrainer.scss";
 
 import PlanningHeader from "@/components/_molecules/PlanningHeader/PlanningHeader.tsx";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -12,10 +12,35 @@ import interactionPlugin from "@fullcalendar/interaction";
 
 function PlanningTrainer() {
 	const [currentView] = useState("dayGridMonth");
+	const [isMobile, setIsMobile] = useState(false);
 
-	const headerToolbar = {
+	// Fonction pour vérifier si l'écran est en mode mobile
+	const checkIsMobile = () => {
+		setIsMobile(window.innerWidth < 850); // valeur en px de $mobile
+	};
+
+	useEffect(() => {
+		// Vérification initiale
+		checkIsMobile();
+
+		// Ajouter l'écouteur d'événement
+		window.addEventListener("resize", checkIsMobile);
+
+		// Nettoyer l'écouteur lors du démontage du composant
+		return () => {
+			window.removeEventListener("resize", checkIsMobile);
+		};
+	}, []);
+
+	const desktopToolbar = {
 		left: "today prev,next title",
 		right: "listWeek,timeGridWeek,dayGridMonth",
+	};
+
+	const mobileToolbar = {
+		left: "listWeek",
+		center: "today,dayGridMonth",
+		right: "prev title next",
 	};
 
 	return (
@@ -30,7 +55,7 @@ function PlanningTrainer() {
 						interactionPlugin,
 					]}
 					initialView={currentView}
-					headerToolbar={headerToolbar}
+					headerToolbar={isMobile ? mobileToolbar : desktopToolbar}
 					locale={frLocale}
 					height="auto"
 					views={{
