50ba7166ab68f73f9a07b23d2f54704f6ac937fa
Florian Schaessens <f.schaessens@gmail.com>
2025-02-19 13:58:23 +0100

    update thin button atom

--- client/src/pages/Owner/Dogs/DogForm/DogForm.tsx
+++ client/src/pages/Owner/Dogs/DogForm/DogForm.tsx
@@ -92,10 +92,11 @@ export default function DogForm({
 
 			const message =
 				mode === "create"
-					? "Votre chien a été ajouté avec succès !"
-					: "Les modifications ont été enregistrées avec succès !";
+					? `Votre chien ${variables.name} a été ajouté avec succès !`
+					: `Les modificationsd de ${variables.name} ont été enregistrées avec succès !`;
 
-			navigate("/owner/my-dogs", { state: { message: `${message}` } });
+			sessionStorage.setItem("dogAlert", message);
+			navigate("/owner/my-dogs");
 		} catch (error) {
 			console.error("Error saving dog:", error);
 		}
@@ -113,12 +114,11 @@ export default function DogForm({
 			<form className="dogForm__form" onSubmit={handleSubmit}>
 				<div>
 					<div className="dogForm__form__title">
-						<div
+						<button
 							className="dogForm__form__title--upload"
 							onClick={() => pictureRef.current?.click()}
 							onKeyDown={handleKeyPress}
-							role="button"
-							tabIndex={0}
+							type="button"
 						>
 							<input
 								type="file"
@@ -126,7 +126,7 @@ export default function DogForm({
 								accept="image/*"
 								ref={pictureRef}
 							/>
-						</div>
+						</button>
 						<div className="dogForm__form__title--intro">
 							<h3>{formTitle}</h3>
 							<p>{formSubtitle}</p>
@@ -138,7 +138,10 @@ export default function DogForm({
 				<TextInput type="birthDate" ref={birthDateRef} inputType="date" />
 				<TextInput type="info" inputType="textarea" ref={infoRef} />
 				<span className="dogForm__button">
-					<Button type="submit" style="thin-btn-light">
+					<Button
+						type="submit"
+						style={{ type: "thin-btn-light", color: "orange" }}
+					>
 						{buttonText}
 					</Button>
 				</span>
