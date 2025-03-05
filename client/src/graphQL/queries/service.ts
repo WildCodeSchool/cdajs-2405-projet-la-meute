0d824052dbf751f0d28bfbdd664b9eabe09f162e
Florence Buchelet <florence.buchelet@gmail.com>
2025-03-05 15:04:42 +0100

    fragment in service queries

--- client/src/graphQL/queries/service.ts
+++ client/src/graphQL/queries/service.ts
@@ -1,34 +1,29 @@
 import { gql } from "@apollo/client";
+import { SERVICE_FRAGMENT } from "../fragments/fragments";
 
 export const GET_ALL_SERVICES = gql`
     query GetAllServices {
         getAllServices {
-            id
-            title
-            smiley
-            color
+            ...ServiceFragment
         }
     }
+    ${SERVICE_FRAGMENT}
 `;
 
 export const GET_SERVICE_BY_ID = gql`
     query GetServiceById($serviceId: Float!) {
         getServiceById(id: $serviceId) {
-            id
-            title
-            smiley
-            color
+            ...ServiceFragment
         }
     }
+    ${SERVICE_FRAGMENT}
 `;
 
 export const GET_SERVICES_BY_EVENT_ID = gql`
     query GetServicesByEventId($eventId: Float!) {
         getServicesByEventId(eventId: $eventId) {
-            id
-            title
-            smiley
-            color
+            ...ServiceFragment
         }
     }
+    ${SERVICE_FRAGMENT}
 `;
