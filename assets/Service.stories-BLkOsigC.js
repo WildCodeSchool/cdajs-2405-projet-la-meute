d9970d96cad7fed819fe2b4373e68fdba68bab78
Dolpheus89 <146729149+Dolpheus89@users.noreply.github.com>
2025-03-06 09:15:46 +0000

    Deploying to gh-pages from @ WildCodeSchool/cdajs-2405-projet-la-meute@2f681b8152b6751d3fb111a2c2f8b986b3be7c20 🚀

---
+++ assets/Service.stories-BLkOsigC.js
@@ -0,0 +1,37 @@
+import{j as h}from"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";function f({service:o,onClick:c}){return h.jsxs("span",{className:"service",style:{backgroundColor:o.color},onClick:c,onKeyUp:c,children:[o.smiley," ",o.title]})}f.__docgenInfo={description:"",methods:[],displayName:"Service",props:{service:{required:!0,tsType:{name:"ServiceType"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const x={title:"atoms/Service",component:f,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{service:{control:{id:"1",color:"#167024",smiley:"🐶",title:"First Service"}}}},e={args:{service:{id:"1",color:"#167024",smiley:"🐶",title:"First Service"}}},r={args:{service:{id:"2",color:"#167024",smiley:"🍟",title:"Second Service"}}},s={args:{service:{id:"3",color:"#000",smiley:"🥞",title:"third Service"}}},i={args:{service:{id:"4",color:"#ff3729",smiley:"🎊",title:"Fourth Service"}}};var t,n,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    service: {
+      id: "1",
+      color: "#167024",
+      smiley: "🐶",
+      title: "First Service"
+    }
+  }
+}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var d,l,m;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    service: {
+      id: "2",
+      color: "#167024",
+      smiley: "🍟",
+      title: "Second Service"
+    }
+  }
+}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var p,v,u;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    service: {
+      id: "3",
+      color: "#000",
+      smiley: "🥞",
+      title: "third Service"
+    }
+  }
+}`,...(u=(v=s.parameters)==null?void 0:v.docs)==null?void 0:u.source}}};var S,g,y;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    service: {
+      id: "4",
+      color: "#ff3729",
+      smiley: "🎊",
+      title: "Fourth Service"
+    }
+  }
+}`,...(y=(g=i.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};const _=["FirstService","SecondService","ThirdService","FourthService"];export{e as FirstService,i as FourthService,r as SecondService,s as ThirdService,_ as __namedExportsOrder,x as default};
