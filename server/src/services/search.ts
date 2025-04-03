31153dc8169847a86ca5be29d313ffb68e81e706
Florence Buchelet <florence.buchelet@gmail.com>
2025-04-03 18:12:36 +0200

    fixed multi word search and stemming issues

--- server/src/services/search.ts
+++ server/src/services/search.ts
@@ -2,10 +2,18 @@ import { dataSource } from "../dataSource/dataSource";
 import { SearchIndex } from "../entities/SearchIndex";
 
 export const search = async (query: string) => {
+	const formattedQuery = query
+		.trim()
+		.split(/\s+/)
+		.map((term) => `${term}:*`)
+		.join(" & ");
+
 	const searchRepo = dataSource.getRepository(SearchIndex);
 
 	return await searchRepo
 		.createQueryBuilder("search")
-		.where("search.document @@ to_tsquery(:query)", { query })
+		.where("search.document @@ to_tsquery('french', :query)", {
+			query: formattedQuery,
+		})
 		.getMany();
 };
