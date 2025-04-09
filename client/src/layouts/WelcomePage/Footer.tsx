13ab2f6b421454923d188b1bc82fde992ded9c6b
Mélissa Parceiller <melissa@mac.home>
2025-04-09 16:06:12 +0200

    fix scroll vers le haut de page au clic sur un lien du footer, ajout d'une fonction scrollToTop

--- client/src/layouts/WelcomePage/Footer.tsx
+++ client/src/layouts/WelcomePage/Footer.tsx
@@ -4,6 +4,10 @@ import Button from "@/components/_atoms/Button/Button";
 import { Link } from "react-router-dom";
 
 function Footer() {
+	const scrollToTop = () => {
+		window.scrollTo(0, 0);
+	};
+
 	return (
 		<>
 			<footer className="welcomepage__footer">
@@ -13,18 +17,28 @@ function Footer() {
 				<div className="welcomepage__footer--lists">
 					<ul className="welcomepage__footer--list">
 						<p className="welcomepage__footer--title">Pages</p>
-						<Link to="/">Accueil</Link>
-						{/*<Link to="/services">Services</Link> */}
-						<Link to="/FAQ">FAQ</Link>
-						<Link to="/contact">Contact</Link>
+						<Link to="/" onClick={scrollToTop}>
+							Accueil
+						</Link>
+						{/*<Link to="/services" onClick={scrollToTop}>Services</Link> */}
+						<Link to="/FAQ" onClick={scrollToTop}>
+							FAQ
+						</Link>
+						<Link to="/contact" onClick={scrollToTop}>
+							Contact
+						</Link>
 					</ul>
 					<ul className="welcomepage__footer--list">
 						<p className="welcomepage__footer--title">Protection des données</p>
-						<Link to="/privacy-policy">Politique de confidentialité</Link>
-						<Link to="/legal-notice">Mentions Légales</Link>
-						{/* <Link to="/offres">Conditions générales de vente</Link>
-						<Link to="/contact">Conditions générales d'utilisation</Link> */}
-						<Link to="/general-terms">
+						<Link to="/privacy-policy" onClick={scrollToTop}>
+							Politique de confidentialité
+						</Link>
+						<Link to="/legal-notice" onClick={scrollToTop}>
+							Mentions Légales
+						</Link>
+						{/* <Link to="/offres" onClick={scrollToTop}>Conditions générales de vente</Link>
+						<Link to="/contact" onClick={scrollToTop}>Conditions générales d'utilisation</Link> */}
+						<Link to="/general-terms" onClick={scrollToTop}>
 							Conditions Générales d'utilisation (CGU)
 						</Link>
 					</ul>
