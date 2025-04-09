2f9595f6a9ff5e37f3341b2fb6ee00112d02b30d
Carcali <144898664+Carcali@users.noreply.github.com>
2025-04-09 07:56:30 +0000

    Deploying to gh-pages from @ WildCodeSchool/cdajs-2405-projet-la-meute@6c1e603bd6262d6ca1ddab54f10f0bada31b67b8 🚀

---
+++ assets/SearchFilters.stories-AKEcNAVH.js
@@ -0,0 +1,6 @@
+import{S as a}from"./SearchFilters-C4emuk3D.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";const l={title:"atoms/SearchFilters",component:a,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{filterOptions:{control:"object"},setFilters:{table:{disable:!0}}}},e={args:{filterOptions:["Chien","Propriétaire","Evènement"],setFilters:()=>{}}};var t,r,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    filterOptions: ["Chien", "Propriétaire", "Evènement"],
+    setFilters: () => {}
+  }
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,l as default};
