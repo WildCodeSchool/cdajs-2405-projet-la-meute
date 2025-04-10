33797add3d8825d7564536d2637c1a706c553351
Florence Buchelet <florence.buchelet@gmail.com>
2025-04-10 10:33:53 +0200

    wip event card

--- client/src/components/_atoms/SearchResultItem/SearchResultItem.tsx
+++ client/src/components/_atoms/SearchResultItem/SearchResultItem.tsx
@@ -1,16 +1,18 @@
 import "./SearchResultItem.scss";
-import type { SearchableEntity } from "@/types/Search";
-import IdCard from "@/components/_molecules/Card/IdCard";
-import type { Owner } from "@/types/User";
+
 import type { Dog } from "@/types/Dog";
-import EventCard from "@/components/_molecules/EventCard/EventCard";
 import type { Event } from "@/types/Event";
+import type { Owner } from "@/types/User";
+import type { SearchableEntity } from "@/types/Search";
+
+import EventCard from "@/components/_molecules/EventCard/EventCard";
+import IdCard from "@/components/_molecules/Card/IdCard";
 
 function SearchResultItem({ entity }: { entity: SearchableEntity }) {
 	const type = entity.__typename.toLowerCase();
 
 	if (type === "owner") {
-		const dogs = (entity as Owner & { dogs: Dog[] }).dogs;
+		const dogs = (entity as unknown as Owner & { dogs: Dog[] }).dogs;
 		return (
 			<div className="searchResultItem__owner">
 				<IdCard type="owner" data={entity as Owner} ownerView={false} />
