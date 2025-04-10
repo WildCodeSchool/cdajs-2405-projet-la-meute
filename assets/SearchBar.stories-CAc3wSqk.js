36f6f399826129413ac07601825335ea4337cb2d
Melprcllr <125665375+Melprcllr@users.noreply.github.com>
2025-04-10 14:41:15 +0000

    Deploying to gh-pages from @ WildCodeSchool/cdajs-2405-projet-la-meute@abd0018326feaa606562cab57d0649017b814231 🚀

---
+++ assets/SearchBar.stories-CAc3wSqk.js
@@ -0,0 +1,10 @@
+import{S as o}from"./SearchBar-CB-Z8are.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";const p={title:"atoms/SearchBar",component:o,tags:["autodocs"],parameters:{layout:"centered",docs:{description:{component:"A search bar component with an input field and a submit button."}}}},t={args:{},parameters:{docs:{description:{story:"Default search bar with placeholder text and submit button"}}}};var e,a,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story: "Default search bar with placeholder text and submit button"
+      }
+    }
+  }
+}`,...(r=(a=t.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const d=["Default"];export{t as Default,d as __namedExportsOrder,p as default};
