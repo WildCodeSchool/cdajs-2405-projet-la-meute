12f1eca8c2e059d29348123c9e9561f500b1a2d3
Julien PRIVAT <contact@julien-privat.com>
2025-04-10 09:34:35 +0200

    Fix du code en français dans useDateFormatter

--- client/src/hooks/useDateFormatter.ts
+++ client/src/hooks/useDateFormatter.ts
@@ -2,13 +2,13 @@
 import { useCallback } from "react";
 
 export const useDateFormatter = () => {
-	// Fonction pour extraire la date au format YYYY-MM-DD
+	// Function to extract date to format YYYY-MM-DD
 	const extractDate = useCallback((dateString: string) => {
 		const date = new Date(dateString);
 		return date.toISOString().split("T")[0];
 	}, []);
 
-	// Fonction pour extraire l'heure au format HH:MM
+	// Function to extract date to format HH:MM
 	const extractTime = useCallback((dateString: string) => {
 		const date = new Date(dateString);
 		return date.toISOString().substring(11, 16);
