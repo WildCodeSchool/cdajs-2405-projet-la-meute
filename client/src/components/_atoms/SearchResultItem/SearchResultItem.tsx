6d22c269f2a05a2546059dc36496e369a7db02b9
Florian Schaessens <f.schaessens@gmail.com>
2025-04-17 08:54:23 +0200

    update link in search items

--- client/src/components/_atoms/SearchResultItem/SearchResultItem.tsx
+++ client/src/components/_atoms/SearchResultItem/SearchResultItem.tsx
@@ -2,7 +2,6 @@ import "./SearchResultItem.scss";
 
 import { Link } from "react-router-dom";
 import { useImageUrl } from "@/hooks/useImageUrl";
-import { useUser } from "@/hooks/useUser";
 import { formatEventDateTime } from "@/helpers/formatEventDate";
 
 import type { Owner, Trainer } from "@/types/User";
@@ -17,7 +16,6 @@ import Service from "@/components/_atoms/Service/Service";
 import DogBubbles from "@/components/_atoms/DogsBubbles/DogsBubbles";
 
 function SearchResultItem({ entity }: { entity: SearchableEntity }) {
-	const { role } = useUser();
 	const type = entity.__typename.toLowerCase();
 
 	if (type === "owner") {
@@ -70,10 +68,7 @@ function SearchResultItem({ entity }: { entity: SearchableEntity }) {
 	const event = entity as unknown as Event & { trainer: Trainer };
 	const dogs = event.participation.map((participation) => participation.dog);
 	return (
-		<Link
-			to={`/${role}/planning/events/${entity.id}`}
-			className="searchResultItem"
-		>
+		<Link to={`/event/${entity.id}`} className="searchResultItem">
 			<span className="searchResultItem__event">
 				<h2 className="searchResultItem__event--title">{event.title}</h2>
 				<span className="searchResultItem__event--trainer">
