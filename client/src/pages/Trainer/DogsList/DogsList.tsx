4da4bbbb582c7c72629baf6256403476adacfe41
Mélissa Parceiller <melissa@mac.home>
2025-04-20 21:06:22 +0200

    affichage des chiens pariticipants pour le trainer connecté

---
+++ client/src/pages/Trainer/DogsList/DogsList.tsx
@@ -0,0 +1,57 @@
+import "./DogsList.scss";
+import PlanningHeader from "@/components/_molecules/PlanningHeader/PlanningHeader";
+import IdCard from "@/components/_molecules/Card/IdCard";
+import { GET_DOGS_BY_TRAINER_EVENTS } from "@/graphQL/queries/participation";
+import { useQuery } from "@apollo/client";
+import { useUser } from "@/hooks/useUser";
+import type { Dog } from "@/types/Dog";
+import { useEffect } from "react";
+
+function DogsList() {
+	const { user } = useUser();
+	const { data, loading, refetch } = useQuery(GET_DOGS_BY_TRAINER_EVENTS, {
+		variables: {
+			trainerId: user?.id ? Number(user.id) : null,
+		},
+		skip: !user?.id,
+	});
+
+	useEffect(() => {
+		if (user?.id) {
+			refetch();
+		}
+	}, [refetch, user?.id]);
+
+	const dogs = data?.getDogsByTrainerEvents || [];
+
+	if (loading) {
+		return <p>loading...</p>;
+	}
+
+	return (
+		<>
+			<PlanningHeader
+				title={"Chiens"}
+				buttonLabel="invite"
+				href="mailto:contact@pawplanner.com"
+			/>
+			<section className="DogsList__infosPage">
+				{dogs.length > 0 ? (
+					<p>
+						Cette liste regroupe les chiens participants à vos évènements,
+						passés ou à venir.
+					</p>
+				) : (
+					<p>Pas encore de chiens participants à vos évènements.</p>
+				)}
+			</section>
+			<section className="DogsList__idCardsDogs">
+				{dogs.map((dog: Dog) => (
+					<IdCard key={dog.id} type="dog" data={dog} trainerView={true} />
+				))}
+			</section>
+		</>
+	);
+}
+
+export default DogsList;
