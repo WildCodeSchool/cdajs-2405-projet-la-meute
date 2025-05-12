81b49793913d10ca61b796239d12dd572042c517
Florence Buchelet <144147299+FlorenceBuchelet@users.noreply.github.com>
2025-05-12 13:48:51 +0200

    Revert "Ajout de react-tooltip et ajout sur DashSidebar et DashHeader"

--- client/src/__tests__/setup.ts
+++ client/src/__tests__/setup.ts
@@ -5,10 +5,3 @@ import { cleanup } from "@testing-library/react";
 afterEach(() => {
 	cleanup();
 });
-
-// Global mock ResizeObserver (need for react-tooltip)
-global.ResizeObserver = class {
-	observe() {}
-	unobserve() {}
-	disconnect() {}
-};
